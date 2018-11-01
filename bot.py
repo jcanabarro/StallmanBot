528e70e8cd8812778d8988e5f5176cacf46b5fd8
jcanabarro <joaovictorcana@gmail.com>
2018-11-01 11:23:03 -0200

    Removing olg notation, and integrate with pyTelegramBotAPI

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import json
 import requests
 import urllib.request
 import shutil
@@ -10,21 +9,6 @@ class BasicActions:
 
     def __init__(self):
         pass
-
-    def get_url(self, url):
-        response = requests.get(url)
-        content = response.content.decode("utf8")
-        return content
-
-    def get_json_from_url(self, url):
-        content = self.get_url(url)
-        js = json.loads(content)
-        return js
-
-    def build_keyboard(self, items):
-        keyboard = [[item] for item in items]
-        reply_markup = {"keyboard": keyboard, "one_time_keyboard": True}
-        return json.dumps(reply_markup)
 
     def get_rms(self, url):
         return str(requests.get(url).content)
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-import time
+import telebot
+from BotConfiguration import BotConfiguration
+from bot import BasicActions
 
-from response import Response
-
-
-def main():
-    bot = Response()
-    last_update_id = None
-    while True:
-        updates = bot.get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = bot.get_last_update_id(updates) + 1
-            bot.handle_updates(updates)
-        time.sleep(1)
+bot = telebot.TeleBot(BotConfiguration.TOKEN)
+bot_basic_action = BasicActions()
 
 
-if __name__ == '__main__':
-    main()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Welcome to the magic Richard Matthew Stallman Bot, you can send /list to "
+                                      "show the available commands")
+
+
+@bot.message_handler(commands=['god', 'nudes', 'stallman', 'rms'])
+def send_photo(message):
+    image_path = bot_basic_action.get_rms("https://rms.sexy/?js")
+    formatted_path = bot_basic_action.format_image_path(image_path)
+    bot_basic_action.download_photo(formatted_path)
+    bot.send_photo(chat_id=message.chat.id, photo=open("." + formatted_path, 'rb'))
+    bot_basic_action.clean_folder()
+
+
+@bot.message_handler(commands=['song'])
+def send_audio(message):
+    bot.send_audio(chat_id=message.chat.id, audio=open("./song/Free Software Song.mp3", 'rb'))
+
+
+@bot.message_handler(commands=['github'])
+def send_github(message):
+    bot.reply_to(message, "My code is available in this github: https://github.com/jcanabarro/StallmanBot")
+
+
+bot.polling()
